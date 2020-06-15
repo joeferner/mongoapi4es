5c6480ffeb9f6e101feb4c7e40169d37881cddae
Joe Ferner <joe@fernsroth.com>
2020-06-15 14:12:46 -0400

    export types in index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { Collection } from './Collection';
+export { Cursor } from './Cursor';
+export { Db } from './Db';
+export { MongoClient } from './MongoClient';
+export { QueryResults } from './QueryResults';
+export * from './types';
