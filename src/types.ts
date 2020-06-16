995384ae4956abd86a9f4cb264757b0e1f255cb5
Joe Ferner <joe@fernsroth.com>
2020-06-16 09:37:48 -0400

    add MongoError to types

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,36 @@
 // see https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/mongodb/index.d.ts
 
-import { MongoError } from 'mongodb';
+/**
+ * Creates a new MongoError
+ * see {@link http://mongodb.github.io/node-mongodb-native/3.5/api/MongoError.html}
+ */
+export interface MongoError extends Error {
+    /**
+     * Checks the error to see if it has an error label
+     */
+    hasErrorLabel(label: string): boolean;
+
+    code?: number;
+    /**
+     * While not documented, the 'errmsg' prop is AFAIK the only way to find out
+     * which unique index caused a duplicate key error. When you have multiple
+     * unique indexes on a collection, knowing which index caused a duplicate
+     * key error enables you to send better (more precise) error messages to the
+     * client/user (eg. "Email address must be unique" instead of "Both email
+     * address and username must be unique") - which caters for a better (app)
+     * user experience.
+     *
+     * Details: https://github.com/Automattic/mongoose/issues/2129 (issue for
+     * mongoose, but the same applies for the native mongodb driver)
+     *
+     * Note that in mongoose (the link above) the prop in question is called
+     * 'message' while in mongodb it is called 'errmsg'. This can be seen in
+     * multiple places in the source code, for example here:
+     * https://github.com/mongodb/node-mongodb-native/blob/a12aa15ac3eaae3ad5c4166ea1423aec4560f155/test/functional/find_tests.js#L1111
+     */
+    errmsg?: string;
+    name: string;
+}
 
 export type RootQuerySelector<T> = {
     /** https://docs.mongodb.com/manual/reference/operator/query/and/#op._S_and */
